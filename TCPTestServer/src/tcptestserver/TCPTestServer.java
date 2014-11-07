d73d101dbf263da01a460cfdc5a65ec0578fa954
Benjamin Nold <brnold@oakland.edu>
2014-11-07 15:01:24 -0500

    added hello world
    becuase it's hello world

--- TCPTestServer/src/tcptestserver/TCPTestServer.java
+++ TCPTestServer/src/tcptestserver/TCPTestServer.java
@@ -15,7 +15,10 @@ public class TCPTestServer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //change a comment
+        
+        System.out.println("Hello world!");
+        
     }
     
 }
